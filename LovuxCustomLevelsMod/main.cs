b5a1e7e07474e29c639ba9d21750f6fe04ebb789
Bud <75859057+bud3699@users.noreply.github.com>
2025-11-01 21:31:26 +0000

    Initial Mod Support

---
+++ LovuxCustomLevelsMod/main.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using System.Reflection;
+
+[assembly: MelonInfo(typeof(LovuxPatcher.Main), "Custom Levels", "0.1.0", "Bud3699")]
+[assembly: MelonGame("Mindlabor", "Lovux")]
+
+namespace LovuxPatcher
+{
+    public class Main : MelonMod
+    {
+        public static string ModVersion;
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Mod Starting!");
+            ModVersion = Info.Version;
+            MelonLogger.Msg("Version: " + ModVersion);
+
+        }
+    }
+}
