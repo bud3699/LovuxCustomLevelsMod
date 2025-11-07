08d0ac495ef8ce5c050f00319f7f7a54ff28ed87
Bud <75859057+bud3699@users.noreply.github.com>
2025-11-07 19:15:10 +0000

    Initial Level Finish Support

---
+++ LovuxCustomLevelsMod/Patches/LevelManagerPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Mindlabor.Utils;
+
+
+namespace LovuxPatcher
+{
+    public static class LevelManagerPatches
+    {
+        [HarmonyPatch(typeof(LevelManager), "CompleteLevelCoroutine")]
+        [HarmonyPostfix]
+        public static void Postfix_LevelManager_CompleteLevelCoroutine()
+        {
+            if (Director.gameMode == GameMode.Game)
+            {
+                AudioManager.instance?.PlaySFX(Director.instance.success);
+                LevelAnimation.isLevelLoading = true;
+                
+
+                //load level co-routine here to reload finished level
+
+            }
+        }
+    }
+}
