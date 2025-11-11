af4346b56cda978d30b51c07c413e3e26cca5752
Bud <75859057+bud3699@users.noreply.github.com>
2025-11-11 01:07:51 +0000

    Achievements Patched

---
+++ LovuxCustomLevelsMod/Patches/AchievementPatcher.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using Steamworks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace LovuxPatcher
+{
+    public static class AchievementPatcher
+    {
+        public static void ApplyPatch(HarmonyLib.Harmony harmony)
+        {
+            var original = AccessTools.Method(typeof(AchievementManager), nameof(AchievementManager.Achieve));
+            var transpiler = AccessTools.Method(typeof(AchievementPatcher), nameof(AchieveTranspiler));
+            harmony.Patch(original, transpiler: new HarmonyMethod(transpiler));
+        }
+
+        public static IEnumerable<CodeInstruction> AchieveTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var levelBuilderField = AccessTools.Field(typeof(Director), "levelBuilder");
+            var customLevelField = AccessTools.Field(typeof(LevelGameBuilder), "customLevel");
+            var directorInstanceGetter = AccessTools.PropertyGetter(typeof(Director), "instance");
+
+            var debugLogMethod = AccessTools.Method(typeof(UnityEngine.Debug), nameof(UnityEngine.Debug.Log), new[] { typeof(object) });
+
+            var newInstructions = new List<CodeInstruction>();
+
+            var continueLabel = new Label();
+
+            newInstructions.Add(new CodeInstruction(OpCodes.Call, directorInstanceGetter));
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldfld, levelBuilderField));
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldfld, customLevelField));
+            newInstructions.Add(new CodeInstruction(OpCodes.Brfalse_S, continueLabel));
+            newInstructions.Add(new CodeInstruction(OpCodes.Ldstr, "blocked achievement"));
+            newInstructions.Add(new CodeInstruction(OpCodes.Call, debugLogMethod));
+            newInstructions.Add(new CodeInstruction(OpCodes.Ret));
+
+            var instructionList = instructions.ToList();
+
+            if (instructionList.Count > 0)
+            {
+                instructionList[0].labels.Add(continueLabel);
+            }
+
+            foreach (var instr in newInstructions)
+                yield return instr;
+
+            foreach (var instr in instructionList)
+                yield return instr;
+        }
+
+    }
+}
